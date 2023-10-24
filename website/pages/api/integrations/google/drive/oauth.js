e1dd9a6bb0664b72ca5ff72075e16c4296754339
M2K Developments <m2ksoftiez@gmail.com>
2023-10-24 22:22:57 +0200

    update integrations api google

--- website/pages/api/integrations/google/drive/oauth.js
+++ website/pages/api/integrations/google/drive/oauth.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         const oAuth2Client = new OAuth2Client(
             process.env.GOOGLE_CLIENT_ID,
             process.env.GOOGLE_CLIENT_SECRET,
-            'https://stickynotespro.m2kdevelopments.com/api/integration/google/drive/callback'
+            'https://stickynotespro.m2kdevelopments.com/api/integrations/google/drive/callback'
         );
 
         // Generate the url that will be used for the consent dialog.
--- website/pages/api/integrations/google/sheets/oauth.js
+++ website/pages/api/integrations/google/sheets/oauth.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         const oAuth2Client = new OAuth2Client(
             process.env.GOOGLE_CLIENT_ID,
             process.env.GOOGLE_CLIENT_SECRET,
-            'https://stickynotespro.m2kdevelopments.com/api/integration/google/sheets/callback'
+            'https://stickynotespro.m2kdevelopments.com/api/integrations/google/sheets/callback'
         );
 
         // Generate the url that will be used for the consent dialog.
